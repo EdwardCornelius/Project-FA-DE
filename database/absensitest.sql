657bd2e15261eafd8ec4fd17fdf3f32382039b0b
unknown <edward@google.com>
2023-04-06 15:10:31 +0700

    database

---
+++ database/absensitest.sql
@@ -0,0 +1,182 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 05, 2023 at 04:17 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `absensitest`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `absensi`
+--
+
+CREATE TABLE `absensi` (
+  `id_absen` int(11) NOT NULL,
+  `tgl` date NOT NULL,
+  `waktu` time NOT NULL,
+  `keterangan` enum('Masuk','Pulang') NOT NULL,
+  `id_user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `absensi`
+--
+
+INSERT INTO `absensi` (`id_absen`, `tgl`, `waktu`, `keterangan`, `id_user`) VALUES
+(27, '2023-03-24', '17:34:49', 'Pulang', 15),
+(28, '2023-03-24', '17:34:57', 'Masuk', 15),
+(29, '2023-03-24', '22:19:48', 'Masuk', 16),
+(30, '2023-03-24', '22:19:53', 'Pulang', 16),
+(31, '2023-04-05', '19:49:29', 'Masuk', 16),
+(32, '2023-04-05', '19:50:22', 'Pulang', 16);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `divisi`
+--
+
+CREATE TABLE `divisi` (
+  `id_divisi` smallint(3) NOT NULL,
+  `nama_divisi` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `divisi`
+--
+
+INSERT INTO `divisi` (`id_divisi`, `nama_divisi`) VALUES
+(1, 'Teknik Komputer dan Jaringan'),
+(2, 'Akuntansi'),
+(3, 'Bisnis Daring dan Pemasaran');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jam`
+--
+
+CREATE TABLE `jam` (
+  `id_jam` tinyint(1) NOT NULL,
+  `start` time NOT NULL,
+  `finish` time NOT NULL,
+  `keterangan` enum('Masuk','Pulang') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `jam`
+--
+
+INSERT INTO `jam` (`id_jam`, `start`, `finish`, `keterangan`) VALUES
+(1, '06:00:00', '07:00:00', 'Masuk'),
+(2, '13:00:00', '15:00:00', 'Pulang');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id_user` smallint(5) NOT NULL,
+  `nik` varchar(20) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `telp` varchar(15) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `foto` varchar(20) DEFAULT 'no-foto.png',
+  `divisi` smallint(5) DEFAULT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  `level` enum('Manager','Karyawan') NOT NULL DEFAULT 'Karyawan'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id_user`, `nik`, `nama`, `telp`, `email`, `foto`, `divisi`, `username`, `password`, `level`) VALUES
+(1, '', 'Edward Cornelius Djongkang', '08139212092', 'edward@gmail.com', '1679744803.jpg', NULL, 'edward', '$2y$10$fGeaw0UBldm3mceDBy/AbeH0wQ1M/fKQuLwR/qsb81618SjRQ6XGC', 'Manager'),
+(8, '8931289124891', 'Manager 1', '', '', 'no-foto.png', NULL, 'manager_1', '$2y$10$XtMY01KEOd5I065s8Exs0OcQ373RvRNG1JznORr6TmmBNWnZ3vjjK', 'Manager'),
+(9, '1231231238900', 'Manager 2', '', '', 'no-foto.png', NULL, 'manager_2', '$2y$10$iJWUOXDznGEmxo.bqnhtmeFL51jN5130LfDlKg8VROfoEmlgC.cFW', 'Manager'),
+(10, '908121310291', 'Manager 3', '', '', 'no-foto.png', NULL, 'manager_3', '$2y$10$uGsLvgl.6ji2iZ7tWkNvPelTwZdLQ6QA81Yawa20wsLairCXqV8BO', 'Manager'),
+(11, '123801204012', 'Manager 4', '', '', 'no-foto.png', NULL, 'master_4', '$2y$10$Kot81WNqrho4WlcYI13kT.Y5V2sMg1ZSAXcITrp8cj3dqHpbl4vrS', 'Manager'),
+(16, '6894', 'Fernando Luis', '089512345678', 'luis@gmail.com', 'no-foto.png', 2, 'luis', '$2y$10$tdMqOi9gpaxubYLeS0.OxO8Wu2xDIqeTcntBnyjnUXPKsALh2zkgm', 'Karyawan'),
+(20, '6893', 'Ericsen Adrian', '08123456789', 'ericsen@gmail.com', 'no-foto.png', 1, 'ericsen', '$2y$10$ey9Z7//xHjGaOONj6.fStu8VsHzGIg.D.FDYdyATAmLeAUugQmnRq', 'Karyawan');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `absensi`
+--
+ALTER TABLE `absensi`
+  ADD PRIMARY KEY (`id_absen`);
+
+--
+-- Indexes for table `divisi`
+--
+ALTER TABLE `divisi`
+  ADD PRIMARY KEY (`id_divisi`);
+
+--
+-- Indexes for table `jam`
+--
+ALTER TABLE `jam`
+  ADD PRIMARY KEY (`id_jam`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `absensi`
+--
+ALTER TABLE `absensi`
+  MODIFY `id_absen` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+
+--
+-- AUTO_INCREMENT for table `divisi`
+--
+ALTER TABLE `divisi`
+  MODIFY `id_divisi` smallint(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `jam`
+--
+ALTER TABLE `jam`
+  MODIFY `id_jam` tinyint(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_user` smallint(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
