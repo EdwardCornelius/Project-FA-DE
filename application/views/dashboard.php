f32daa8d3fd31db12a11cbeef3f8d82e407a9067
unknown <edward@google.com>
2023-04-06 14:36:52 +0700

    Project-FA-DE

---
+++ application/views/dashboard.php
@@ -0,0 +1,35 @@
+<div class="jumbotron bg-primary text-white" style="position:relative">
+    <h4 class="my-0">Selamat datang di</h4>
+    <h1 class="display-4 my-0">Aplikasi Absensi Online</h1>
+    <hr class="my-4">
+    <p class="lead">Aplikasi absensi online berbasis website SMK Kristen Immanuel</p>
+
+    <div class="circle" style="
+    background-color:#fff;
+    opacity:0.1;
+    position:absolute;
+    width:230px;
+    height:230px;
+    top:10px;
+    right:20px;
+    border-radius:50%;
+    "></div>
+      <div class="circle" style="
+    background-color:#fff;
+    opacity:0.1;
+    position:absolute;
+    width:130px;
+    height:130px;
+    top:5px;
+    left:10px;
+    border-radius:50%;
+    "></div>
+</div>
+<div class="row">
+                        <div class="col-6">
+                            <img style="width:100%; height:250px" class="img-fluid rounded" src="<?php echo base_url('assets/img/banner1.jpg') ?>">
+                        </div>
+                        <div class="col-6">
+                            <img style="width:100%; height:250px" class="img-fluid rounded" src="<?php echo base_url('assets/img/banner2.jpg') ?>">
+                        </div>
+                    </div>
